f7d40e03bb8e3685e37f63fbfa015c33b90b5591
leejend <jeanettelee09@optusnet.com.au>
2018-10-09 23:25:10 +1100

    Initial Commit

---
+++ Tutorial 8_Q6/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tutorial 8_Q6
+//
+//  Created by Jeanette Lee on 10/9/18.
+//  Copyright © 2018 Jeanette Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
